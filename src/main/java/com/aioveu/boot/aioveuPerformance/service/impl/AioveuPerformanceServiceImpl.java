6f86721d87be139bd0348b55c2234ceb3f361a30
ambitiouschild <ambitiouschild@qq.com>
2025-08-18 23:26:17 +0800

    【aioveuPerformance】员工绩效考评表CRUD【2.0】

---
+++ src/main/java/com/aioveu/boot/aioveuPerformance/service/impl/AioveuPerformanceServiceImpl.java
@@ -0,0 +1,125 @@
+package com.aioveu.boot.aioveuPerformance.service.impl;
+
+import com.aliyun.oss.ServiceException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.aioveu.boot.aioveuPerformance.mapper.AioveuPerformanceMapper;
+import com.aioveu.boot.aioveuPerformance.service.AioveuPerformanceService;
+import com.aioveu.boot.aioveuPerformance.model.entity.AioveuPerformance;
+import com.aioveu.boot.aioveuPerformance.model.form.AioveuPerformanceForm;
+import com.aioveu.boot.aioveuPerformance.model.query.AioveuPerformanceQuery;
+import com.aioveu.boot.aioveuPerformance.model.vo.AioveuPerformanceVO;
+import com.aioveu.boot.aioveuPerformance.converter.AioveuPerformanceConverter;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import cn.hutool.core.lang.Assert;
+import cn.hutool.core.util.StrUtil;
+
+/**
+ * 员工绩效考评服务实现类
+ *
+ * @author 可我不敌可爱
+ * @since 2025-08-18 22:48
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AioveuPerformanceServiceImpl extends ServiceImpl<AioveuPerformanceMapper, AioveuPerformance> implements AioveuPerformanceService {
+
+    private final AioveuPerformanceConverter aioveuPerformanceConverter;
+
+    /**
+    * 获取员工绩效考评分页列表
+    *
+    * @param queryParams 查询参数
+    * @return {@link IPage<AioveuPerformanceVO>} 员工绩效考评分页列表
+    */
+    @Override
+    public IPage<AioveuPerformanceVO> getAioveuPerformancePage(AioveuPerformanceQuery queryParams) {
+        Page<AioveuPerformanceVO> pageVO = this.baseMapper.getAioveuPerformancePage(
+                new Page<>(queryParams.getPageNum(), queryParams.getPageSize()),
+                queryParams
+        );
+        return pageVO;
+    }
+    
+    /**
+     * 获取员工绩效考评表单数据
+     *
+     * @param id 员工绩效考评ID
+     * @return 员工绩效考评表单数据
+     */
+    @Override
+    public AioveuPerformanceForm getAioveuPerformanceFormData(Long id) {
+        AioveuPerformance entity = this.getById(id);
+        return aioveuPerformanceConverter.toForm(entity);
+    }
+    
+    /**
+     * 新增员工绩效考评
+     *
+     * @param formData 员工绩效考评表单对象
+     * @return 是否新增成功
+     */
+    @Override
+    public boolean saveAioveuPerformance(AioveuPerformanceForm formData) {
+        AioveuPerformance entity = aioveuPerformanceConverter.toEntity(formData);
+        return this.save(entity);
+    }
+    
+    /**
+     * 更新员工绩效考评
+     *
+     * @param id   员工绩效考评ID
+     * @param formData 员工绩效考评表单对象
+     * @return 是否修改成功
+     */
+    @Override
+    public boolean updateAioveuPerformance(Long id,AioveuPerformanceForm formData) {
+        log.info("开始更新绩效考评记录: ID={}", id);
+
+        // 1. 获取原始绩效考评信息
+        AioveuPerformance original = getById(id);
+        if (original == null) {
+            log.error("绩效考评记录不存在: ID={}", id);
+            throw new ServiceException("绩效考评记录不存在");
+        }
+
+        // 3. 将表单数据转换为实体对象
+        AioveuPerformance entity = aioveuPerformanceConverter.toEntity(formData);
+
+        // 4. 设置绩效考评ID
+        entity.setId(id);
+        entity.setCreateTime(original.getCreateTime());
+        //performance_grade是根据其他字段计算得出的
+
+
+        // 6. 执行更新 // 6. 使用UpdateWrapper更新，排除生成列  updateWithWrapper(entity);
+        return this.updateById(entity);
+    }
+
+    /**
+     * 删除员工绩效考评
+     *
+     * @param ids 员工绩效考评ID，多个以英文逗号(,)分割
+     * @return 是否删除成功
+     */
+    @Override
+    public boolean deleteAioveuPerformances(String ids) {
+        Assert.isTrue(StrUtil.isNotBlank(ids), "删除的员工绩效考评数据为空");
+        // 逻辑删除
+        List<Long> idList = Arrays.stream(ids.split(","))
+                .map(Long::parseLong)
+                .toList();
+        return this.removeByIds(idList);
+    }
+
+}
